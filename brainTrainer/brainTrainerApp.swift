cf21bf4c21d65045cbddeb2afe1bcc412888a178
Hugo Peyron <hugopeyron@gmail.com>
2024-06-02 21:31:43 +0200

    Initial Commit

---
+++ brainTrainer/brainTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainTrainerApp.swift
+//  brainTrainer
+//
+//  Created by Hugo Peyron on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct brainTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
